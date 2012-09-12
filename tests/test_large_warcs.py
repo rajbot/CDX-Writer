d6c3bb4b4a5b362eb06e0a407208978eeb0c3631
rajbot <raj@archive.org>
2012-09-12 19:16:19 +0000

    Add test case for an ARC file missing a filedesc: header

--- tests/test_large_warcs.py
+++ tests/test_large_warcs.py
@@ -9,6 +9,7 @@ warc_dir = '/warcs/'
 warcs = {'YTV-20120204025848-crawl442/YTV-20120204035110-15431.warc.gz':                       '8be3352ac814c58e333d1a179e7e7951',
          'WIDE-20120121162724-crawl411/WIDE-20120121174231-03025.warc.gz':                     'c8e315322fa22f84f93f0abd0d414281',
          'live-20120312105341306-00165-20120312171822397/live-20120312161414739-00234.arc.gz': '13dfd294b80d43696e67f60c8f87eb1e',
+         'wb_urls.ia11013.20050517055850-c/wb_urls.ia11013.20050805040525.arc.gz':             'f29414651eaced77184a048a2399477c', #missing filedesc:// header
         }
 
 for file, hash in warcs.iteritems():
